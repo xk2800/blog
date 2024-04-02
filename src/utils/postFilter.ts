c2ae91b8a2890f23bfdbb98798018bbcedfe7fef
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-04-02 22:00:42 +0800

    add recent posts on homepage

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -3,7 +3,7 @@ import type { CollectionEntry } from "astro:content";
 
 const postFilter = ({ data }: CollectionEntry<"posts">) => {
   const isPublishTimePassed =
-    Date.now() > new Date(data.pubDate).getTime() - 15 * 60 * 1000;
+    Date.now() > new Date(data.pubDate).getTime() - 15 * 60 * 1000; // 15 mins
   return import.meta.env.DEV || isPublishTimePassed;
 };
 
